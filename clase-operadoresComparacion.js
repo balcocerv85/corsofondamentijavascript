909185a554f0c630c5ca34c82d09c86a8aeec611
balcocerv85 <bolivar.alcocerv@gmail.com>
2024-09-04 17:06:13 +0200

    aggiunta clase operadores de comparacion

---
+++ clase-operadoresComparacion.js
@@ -0,0 +1,38 @@
+/*
+
+Operadores de Comparaciòn: Los operadores de comparación en JavaScript se utilizan para comparar dos valores y devolver 
+un resultado booleano (true o false).
+
+*/
+
+// Igualdad (==):Compara dos valores para ver si son iguales, pero sin tener en cuenta el tipo de datos.
+5 == "5" // true
+
+
+// Igualdad estricta (===): Compara dos valores para ver si son iguales y del mismo tipo de datos.
+5 === "5" // false
+5 === 5 // true
+
+// Desigualdad (!=): Compara dos valores para ver si son diferentes, pero sin tener en cuenta el tipo de datos.
+5 != "5" // false
+5 != 6 // true
+
+// Desigualdad estricta (!==): Compara dos valores para ver si son diferentes o de diferentes tipos de datos.
+5 !== "5" // true 
+5 !== 5 // false
+
+// Mayor que (>): Verifica si el valor de la izquierda es mayor que el valor de la derecha.
+6 > 5 // true 
+5 > 5 // false
+
+// Mayor o igual que (>=): Verifica si el valor de la izquierda es mayor o igual que el valor de la derecha.
+6 >= 5 // true 
+5 >= 5 // true
+
+// Menor que (<): Verifica si el valor de la izquierda es menor que el valor de la derecha.
+4 < 5 // true 
+5 < 5 // false
+
+// Menor o igual que (<=):Verifica si el valor de la izquierda es menor o igual que el valor de la derecha.
+4 <= 5 // true 
+5 <= 5 // true
